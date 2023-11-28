c5bc2e74484ee159d2a589d7989d06461813904b
Evangelos Papakirykou <vpapakir@gmail.com>
2023-11-28 22:23:25 +0000

    fixed

--- vpapakirscalasbttemplate/vpapakirscalasbttemplate-common/src/main/scala/common/vpapakir/tools/ThreadFactory.scala
+++ vpapakirscalasbttemplate/vpapakirscalasbttemplate-common/src/main/scala/common/vpapakir/tools/ThreadFactory.scala
@@ -19,10 +19,12 @@ package common.vpapakir.tools
 
 class ThreadFactory extends Thread  
 { 
+    /*
     override def run() 
     { 
         // Displaying the thread that is running  
         println("Thread " + Thread.currentThread().getName() +  
                                         " is running.") 
-    } 
+    }
+    */
 }  
